a1ebb2339bb5065b7a49bc06cbe8cd426a128ec4
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-07-04 19:35:12 +0530

    add container styles and animation

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Container, CtaButton } from '../../styles/common'
 
-const Container = styled.div``
+const HeaderWrapper = styled.section`
+    min-height: 100vh;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		border-bottom: 0;
+		background: #fff;
+		font-weight: 400;
+		color: #272341;
+		padding: 0 5.6rem;
+		margin-bottom: 0;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		z-index: -1;
+		font-size: 40px;
+	.text-color-main{
+		background-image: linear-gradient(135deg,#02aab0,#00cdac);
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+`
 
-const Fade = styled.div``
+const Fade = styled.div`
+  left: 0px;
+	position: relative;
+	animation-name: slide;
+	animation-duration: 1s;
+  transition: all 550ms cubic-bezier(0.02, 0.01, 0.47, 1);
+	/* color: ${props =>
+		props.side == 'left' ? 'left' :
+			props.variant == 'right' ? 'right' :
+				''
+	}; */
+	@keyframes slide {
+		from { transform: translateX(-2rem); opacity: 0; }
+		to { transform: translateX(0rem); opacity: 1; }
+	}
+`
 
 
 const Header = () => {
 	return (
-		<section id="hero" className="jumbotron">
+		<HeaderWrapper id="hero" className="jumbotron">
 			<Container>
 				<Fade>
 					<h1 className="hero-title">
@@ -19,15 +58,15 @@ const Header = () => {
 				</Fade>
 				<Fade>
 					<p className="hero-cta">
-						<span className="cta-btn cta-btn--hero">
+						<CtaButton className="cta-btn cta-btn--hero">
 							{/* <Link to="about" smooth duration={1000}>
-                Know more
               </Link> */}
-						</span>
+							Know more
+						</CtaButton>
 					</p>
 				</Fade>
 			</Container>
-		</section>
+		</HeaderWrapper>
 	);
 };
 
--- src/styles/common.js
+++ src/styles/common.js
@@ -6,6 +6,54 @@ const Container = styled.div`
 	padding-left: 15px;
 	margin-right: auto;
 	margin-left: auto;
+	@media (max-width: 992px){
+		max-width: 960px;
+	}
+	@media (max-width: 768px){
+		max-width: 720px;
+	}
+	@media (max-width: 576px){
+    max-width: 540px;
+	}
 `;
 
-export { Container };
+const CtaButton = styled.button`
+	background-image: linear-gradient(135deg,#02aab0,#00cdac);
+	background-clip: text;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	border-width: 2px;
+	border-style: solid;
+	border-image: linear-gradient(135deg,#02aab0,#00cdac);
+	-o-border-image: linear-gradient(135deg,#02aab0,#00cdac);
+	border-image-slice: 1;
+	cursor: pointer;
+	display: inline-block;
+	position: relative;
+	padding: 0.8rem 1.6rem;
+	font-weight: 700;
+	line-height: 1;
+	z-index: 1;
+	transition: all .6s cubic-bezier(.19,1,.22,1);
+	&:hover {
+    -webkit-text-fill-color: #fff;
+    text-decoration: none;
+	}
+	&::after{
+		background-image: linear-gradient(135deg,#02aab0,#00cdac);
+		content: "";
+    display: block;
+    position: absolute;
+    width: 0;
+    height: 100%;
+    left: 0;
+    bottom: 0;
+    z-index: -1;
+    transition: all .3s cubic-bezier(.19,1,.22,1);
+		&:hover{
+			width: 100%;
+		}
+	}
+`
+
+export { Container, CtaButton };
